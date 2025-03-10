439d2cb5184a39a6122b2f2af836d223a290d99d
Fakipo <paraskavdikar@gmail.com>
2025-03-09 17:24:39 -0700

    Added maven directory structure

---
+++ src/main/java/helper/MathHelper.java
@@ -0,0 +1,23 @@
+package helper;
+
+public class MathHelper {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return a / b;
+    }
+}
